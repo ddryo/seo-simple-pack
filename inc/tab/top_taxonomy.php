6b53a0d04e571908dc1b46e4e6649abcaa5daa77
ddryo <dd.bocsfcryo@gmail.com>
2022-10-05 16:16:53 +0900

    投稿フォーマットの設定は、テーマサポートが有効化中のみ表示

--- inc/tab/top_taxonomy.php
+++ inc/tab/top_taxonomy.php
@@ -83,28 +83,31 @@ foreach ( SSP_Data::$custom_taxonomies as $tax_name => $tax_label ) {
 
 
 // 投稿フォーマット
-$tax_title    = __( 'Post Format', 'loos-ssp' );
-$quoted_title = sprintf( SSP_Data::$texts['quoted_archive_title'], $tax_title );
-self::output_section( $tax_title, [
-	'post_format_disable' => [
-		'title' => sprintf( SSP_Data::$texts['nouse'], $quoted_title ),
-		'type'  => 'switch',
-		'desc'  => sprintf( __( 'If you select "Yes", you will be redirected to the Home even if you access %s.', 'loos-ssp' ), $quoted_title ),
-	],
-	'post_format_noindex' => [
-		'title' => sprintf( SSP_Data::$texts['noindex'], $quoted_title ),
-		'type'  => 'switch',
-		'desc'  => SSP_Data::$texts['noindex_help'],
-	],
-	'post_format_title' => [
-		'title'       => SSP_Data::$texts['title_tag'],
-		'preview'     => true,
-		'desc'        => sprintf( SSP_Data::$texts['default_output'], $quoted_title, '<code>&lt;title&gt;</code>' ),
-	],
-	'post_format_desc' => [
-		'title'       => SSP_Data::$texts['description_tag'],
-		'class'       => 'sep',
-		'preview'     => true,
-		'desc'        => sprintf( SSP_Data::$texts['default_output'], $quoted_title, '<code>meta:description</code>' ),
-	],
-] );
+$supported_formats = get_theme_support( 'post-formats' );
+if ( ! empty( $supported_formats ) ) {
+	$tax_title    = __( 'Post Format', 'loos-ssp' );
+	$quoted_title = sprintf( SSP_Data::$texts['quoted_archive_title'], $tax_title );
+	self::output_section( $tax_title, [
+		'post_format_disable' => [
+			'title' => sprintf( SSP_Data::$texts['nouse'], $quoted_title ),
+			'type'  => 'switch',
+			'desc'  => sprintf( __( 'If you select "Yes", you will be redirected to the Home even if you access %s.', 'loos-ssp' ), $quoted_title ),
+		],
+		'post_format_noindex' => [
+			'title' => sprintf( SSP_Data::$texts['noindex'], $quoted_title ),
+			'type'  => 'switch',
+			'desc'  => SSP_Data::$texts['noindex_help'],
+		],
+		'post_format_title' => [
+			'title'       => SSP_Data::$texts['title_tag'],
+			'preview'     => true,
+			'desc'        => sprintf( SSP_Data::$texts['default_output'], $quoted_title, '<code>&lt;title&gt;</code>' ),
+		],
+		'post_format_desc' => [
+			'title'       => SSP_Data::$texts['description_tag'],
+			'class'       => 'sep',
+			'preview'     => true,
+			'desc'        => sprintf( SSP_Data::$texts['default_output'], $quoted_title, '<code>meta:description</code>' ),
+		],
+	] );
+}
