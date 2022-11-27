af0d41147b47edaeea63b1a31d5478d52518a5ad
ddryo <dd.bocsfcryo@gmail.com>
2022-11-27 21:13:24 +0900

    delete space

--- inc/tab/top_webmaster.php
+++ inc/tab/top_webmaster.php
@@ -15,10 +15,10 @@ foreach ( $settings  as $key => $data ) {
 	$desc = __( 'Output code ', 'loos-ssp' ) .
 		' : <code>&lt;meta name="' . $data[1] . '" content="<b>' . _x( 'Your code', 'input', 'loos-ssp' ) . '</b>"&gt;</code>';
 
-		$section_args[ $key ] = [
-			'title'       => sprintf( __( 'Authentication code for %s', 'loos-ssp' ), $data[0] ),
-			'desc'        => $desc,
-			'class'       => '-wide -webmaster',
-		];
+	$section_args[ $key ] = [
+		'title'       => sprintf( __( 'Authentication code for %s', 'loos-ssp' ), $data[0] ),
+		'desc'        => $desc,
+		'class'       => '-wide -webmaster',
+	];
 }
 self::output_section( __( 'Webmaster Tools Certification Code', 'loos-ssp' ), $section_args );
